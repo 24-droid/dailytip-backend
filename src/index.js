d367ffd307fafdc9321e680041c70e7c5f5a0ddf
Om Singh <omsingh06102005@gmail.com>
2025-02-05 20:01:43 +0530

    Made changes in the interval

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ setInterval(() => {
     const hours = now.getHours();  
     const minutes = now.getMinutes();
     
-    if (hours === 20 && minutes === 0) {
+    if (hours === 20 && minutes === 5) {
       sendDailyArticles();
     }
   }, 60000);
